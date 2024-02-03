df437c1af955331cd5af2f9145570440037426e6
Mostafa-Elsisi <elsisimostafa@gmail.com>
2024-02-03 21:50:26 +0200

    Solve problem C

---
+++ Sheet #1 (Data type - Conditions)/C. Simple Calculator/C. Simple Calculator.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    long long num1=0;
+    long long num2=0;
+    scanf("%lld%lld",&num1,&num2);
+    printf("%lld + %lld = %lld\n",num1,num2,num1+num2);
+    printf("%lld * %lld = %lld\n",num1,num2,num1*num2);
+    printf("%lld - %lld = %lld\n",num1,num2,num1-num2);
+}
